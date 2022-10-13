b472b507eb4748f4e8538fff892bb2ef2fb6f902
Sofiia <sofiakazzz@gmail.com>
2022-10-13 20:46:14 +0300

    The  program  reads a sequence of integer values from standard input and finds the average value.

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -1,11 +1,27 @@
 package com.epam.rd.autotasks;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int input = 0;
+        int sum = 0;
+        int avarage_number = 0;
+        int counter = 0;
         // Use Scanner methods to read input
+        while ((input = scanner.nextInt()) != 0) {
+           sum += input;
+           counter++;
+            }
+            if (counter == 0) {
+                System.out.println("The element was not entered.");
+            }
+            if (counter > 0) {
+                avarage_number = sum/ counter;
+                System.out.println(avarage_number);
+            }
 
     }
 
